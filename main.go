3aee4678af37dd148c283577a49032f67e527e5c
tariqislam <tariqislam@tariqislam-macbookpro.roam.corp.google.com>
2018-04-24 13:54:17 -0400

    updating output message to be url path

--- main.go
+++ main.go
@@ -4,76 +4,76 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "log"
-    "net/http"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func listDir() {
-    files, err := ioutil.ReadDir("./")
-    if err != nil {
-        log.Fatal(err)
-    }
+	files, err := ioutil.ReadDir("/etc/")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, f := range files {
-            fmt.Println(f.Name())
-    }
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
 
-func fileWrite() {
+func fileWrite(logURL string) {
 
-    // To start, here's how to dump a string (or just
-    // bytes) into a file.
-    // d1 := []byte("hello\ngo\n")
-    // err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
-    // check(err)
+	// To start, here's how to dump a string (or just
+	// bytes) into a file.
+	// d1 := []byte("hello\ngo\n")
+	// err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
+	// check(err)
 
-    // For more granular writes, open a file for writing.
-    f, err := os.Create("/etc/datafile")
-    check(err)
+	// For more granular writes, open a file for writing.
+	f, err := os.Create("/etc/datafile")
+	check(err)
 
-    // It's idiomatic to defer a `Close` immediately
-    // after opening a file.
-    defer f.Close()
+	// It's idiomatic to defer a `Close` immediately
+	// after opening a file.
+	defer f.Close()
 
-    // You can `Write` byte slices as you'd expect.
-    // d2 := []byte{115, 111, 109, 101, 10}
-    // n2, err := f.Write(d2)
-    // check(err)
-    // fmt.Printf("wrote %d bytes\n", n2)
+	// You can `Write` byte slices as you'd expect.
+	// d2 := []byte{115, 111, 109, 101, 10}
+	// n2, err := f.Write(d2)
+	// check(err)
+	// fmt.Printf("wrote %d bytes\n", n2)
 
-    // A `WriteString` is also available.
-    //n3, err := f.WriteString("writes\n")
-    // fmt.Printf("wrote %d bytes\n", n3)
+	// A `WriteString` is also available.
+	//n3, err := f.WriteString("writes\n")
+	// fmt.Printf("wrote %d bytes\n", n3)
 
-    // Issue a `Sync` to flush writes to stable storage.
-    //f.Sync()
+	// Issue a `Sync` to flush writes to stable storage.
+	//f.Sync()
 
-    // `bufio` provides buffered writers in addition
-    // to the buffered readers we saw earlier.
-    w := bufio.NewWriter(f)
-    n4, err := w.WriteString("well, hello there!\n")
-    fmt.Printf("wrote %d bytes\n", n4)
+	// `bufio` provides buffered writers in addition
+	// to the buffered readers we saw earlier.
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString(logURL)
+	fmt.Printf("wrote %d bytes\n", n4)
 
-    // Use `Flush` to ensure all buffered operations have
-    // been applied to the underlying writer.
-    w.Flush()
-    listDir()
+	// Use `Flush` to ensure all buffered operations have
+	// been applied to the underlying writer.
+	w.Flush()
+	//listDir()
 
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love persistent apps on GKE %s!\n", r.URL.Path[1:])
-	fileWrite()
+	fileWrite(r.URL.Path[1:])
 }
 
 func main() {
