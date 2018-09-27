8e149f2a6fcf776a05be7a7398c452c3266515fc
tariqislam@google.com <tariqislam@google.com>
2018-09-27 09:03:02 -0400

    make init more readable

--- skaffold-up.sh
+++ skaffold-up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && chmod +x skaffold && sudo mv skaffold /usr/local/bin
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+chmod +x skaffold && sudo mv skaffold /usr/local/bin
