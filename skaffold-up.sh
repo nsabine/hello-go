a6d4aa39353fb63da334516ba51c6550b102ed33
tariqislam@google.com <tariqislam@google.com>
2018-09-27 08:59:17 -0400

    updating skaffold init

--- skaffold-up.sh
+++ skaffold-up.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/tariq-islam/hello-go && cd hello-go
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && chmod +x skaffold && sudo mv skaffold /usr/local/bin
